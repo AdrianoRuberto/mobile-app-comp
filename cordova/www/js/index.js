f6f73ab67c8ba5b95941131131694d6b6cddb21c
Adriano Ruberto <adriano.ruberto@welitics.com>
2018-10-05 02:45:28 +0200

    finish cordova implementation

--- cordova/www/js/index.js
+++ cordova/www/js/index.js
@@ -1,74 +1,39 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function () {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
-
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function () {
-        this.receivedEvent('deviceready');
-    },
-
-    // Update DOM on a Received Event
-    receivedEvent: function (id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
-};
-
-app.initialize();
+var todos = {};
+var onlyShowNotDone = false;
 
 function addTodo() {
     var ul = document.getElementById("list");
+    var key = ul.childNodes.length;
     var swtch = document.createElement("label");
+    var li = document.createElement("li");
     swtch.setAttribute("class", "switch")
+
     var input = document.createElement("input");
     input.setAttribute("id", "toggle");
-    input.setAttribute("type", "checkbox")
+    input.setAttribute("type", "checkbox");
+    input.addEventListener("click", function () {toggleItem(key, li)});
+
+    todos[key] = false;
+
     var span = document.createElement("span");
     span.setAttribute("class", "slider round");
 
     swtch.appendChild(input);
     swtch.appendChild(span);
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode("Todo item#" + ul.childNodes.length));
+    li.appendChild(document.createTextNode("Todo item#" + key));
     li.appendChild(swtch);
     ul.appendChild(li);
-    li.onclick = toggleItem;
 }
 
-function toggleItem(e) {
-    e.target.childNodes.getElementById("toggle").setAttribute("")
+function toggleItem(key, li) {
+    todos[key] = !todos[key];
+    if (onlyShowNotDone) {
+        li.setAttribute("hidden", true);
+    }
 }
 
 function filteredTodos() {
-    document.getElementsByTagName("li")
-    this.todos.filter((todo) => !(todo.done && this.onlyShowNotDone));
+    onlyShowNotDone = !onlyShowNotDone;
+    Array.from(document.querySelectorAll("li"))
+        .map((l, i) => (todos[i] && onlyShowNotDone) ? l.setAttribute("hidden", true) : l.removeAttribute("hidden"));
 }
