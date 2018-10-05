69b4be792265b0313f275935dc3fe43b86988099
Adriano Ruberto <adriano.ruberto@welitics.com>
2018-10-05 08:26:32 +0200

    Fix statusbar being broken on iOS

--- expo/App.js
+++ expo/App.js
@@ -19,28 +19,26 @@ export default class App extends Component {
 
   render() {
     return (
-      <View>
+      <View style={styles.container}>
         <View style={styles.statusBar} />
-        <View style={styles.container}>
-          <Text style={styles.title}>React Native + Expo Todo List</Text>
-          <View style={styles.options}>
-            <Button
-              onPress={this.addTodo}
-              title="+ Add a todo"
-              color="blue"
+        <Text style={styles.title}>React Native + Expo Todo List</Text>
+        <View style={styles.options}>
+          <Button
+            onPress={this.addTodo}
+            title="+ Add a todo"
+            color="blue"
+          />
+          <View style={styles.hide}>
+            <Text>Hide done</Text>
+            <Switch
+              onValueChange={(value) => this.setState({onlyShowNotDone: value})}
+              value={this.state.onlyShowNotDone}
             />
-            <View style={styles.hide}>
-              <Text>Hide done</Text>
-              <Switch
-                onValueChange={(value) => this.setState({onlyShowNotDone: value})}
-                value={this.state.onlyShowNotDone}
-              />
-            </View>
           </View>
-          <FlatList
-            data={this.state.todos.filter((todo) => !(todo.done && this.state.onlyShowNotDone))}
-            renderItem={({item}) => this.renderTodo(item)} />
         </View>
+        <FlatList
+          data={this.state.todos.filter((todo) => !(todo.done && this.state.onlyShowNotDone))}
+          renderItem={({item}) => this.renderTodo(item)} />
       </View>
     );
   }
@@ -80,7 +78,7 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
   statusBar: {
-    backgroundColor: "black",
+    backgroundColor: "transparent",
     minHeight: Constants.statusBarHeight,
   },
   title: {
